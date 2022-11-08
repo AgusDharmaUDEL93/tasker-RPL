b1f704a923f1b751c1e80f616a5d6ffa99be69fc
e-meyer <edmundom@mx2.unisc.br>
2022-11-07 23:36:48 -0300

    feat: create route_generator to handle navigation and created constants insite the core layer to store the navigation routes

---
+++ lib/route_generator.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:tasker/layers/core/utils/constants.dart';
+import 'layers/app/views/home/home_view.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case kHomeRoute:
+        return MaterialPageRoute(builder: (_) => const HomePage());
+      default:
+        return errorRoute();
+    }
+  }
+}
+
+MaterialPageRoute errorRoute() {
+  return MaterialPageRoute(
+    builder: (_) => const Scaffold(
+      body: Center(
+        child: Text('No route defined'),
+      ),
+    ),
+  );
+}
