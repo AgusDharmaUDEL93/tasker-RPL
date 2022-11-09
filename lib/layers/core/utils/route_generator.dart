899cfcf521968be293152702907707b3eafcc423
e-meyer <edmundom@mx2.unisc.br>
2022-11-09 19:50:48 -0300

    feat: add route for create_task_page

--- lib/layers/core/utils/route_generator.dart
+++ lib/layers/core/utils/route_generator.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:tasker/layers/app/views/ui/create_task/create_task_page.dart';
 import 'package:tasker/layers/core/utils/constants.dart';
-import '../../app/views/ui/home/home_view.dart';
+import '../../app/views/ui/home/home_page.dart';
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case kHomeRoute:
         return MaterialPageRoute(builder: (_) => const HomePage());
+      case kCreateTaskRoute:
+        return MaterialPageRoute(builder: (_) => const CreateTaskPage());
+
       default:
         return errorRoute();
     }
