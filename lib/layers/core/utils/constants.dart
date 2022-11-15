12155b28d2efad1a8d47ee74f1244ddc0e77a441
e-meyer <edmundom@mx2.unisc.br>
2022-11-15 18:40:17 -0300

    feat: add animation to line through text after marking task as done

--- lib/layers/core/utils/constants.dart
+++ lib/layers/core/utils/constants.dart
@@ -5,23 +5,29 @@ const String kHomeRoute = '/';
 const String kManageTaskRoute = '/manage-task';
 const String kSearchTaskRoute = '/search-task';
 
-// Color Pallete
+// App Color Pallete
 const Color kStroke = Color(0xFFE9F1FF);
 const Color kLightBlue = Color(0xFFE9F1FF);
 const Color kPrimaryColor = Color(0xFFF05454);
 const Color kSecondaryColor = Color(0xFF30475E);
 const Color kDarkGrey = Color(0xFF848A94);
 const Color kLightGrey = Color(0xFFbebebe);
-const Color kCardBackground = Color(0xFFEEEEEE);
-const Color kCardCollapsedBackground = Color(0xFFdbd9d9);
+const Color kCardBackground = Color(0xFFf8f8f8);
+const Color kCardCollapsedBackground = Color(0xFFECECEC);
 const Color kMainBackground = Color(0xFFFFFFFF);
+
+// Card Flags
+const Color kGreenCardFlag = Color(0xFF07da63);
+const Color kYellowCardFlag = Color(0xFFFEDC56);
+const Color kRedCardFlag = Color(0xFFee6b6e);
 
 // Dark Mode
 // const Color kStroke = Color(0xFFE9F1FF);
-// const Color kLightBlue = Color(0xFFE9F1FF);
+// const Color kLightBlue = Color(0xFF30475E);
 // const Color kPrimaryColor = Color(0xFFF05454);
 // const Color kSecondaryColor = Color(0xFF30475E);
 // const Color kDarkGrey = Color(0xFF848A94);
 // const Color kLightGrey = Color(0xFFbebebe);
 // const Color kCardBackground = Color(0xFF30475E);
+// const Color kCardCollapsedBackground = Color(0xFF30475E);
 // const Color kMainBackground = Color(0xFF222831);
--- lib/layers/presentation/ui/widgets/task_card_widget.dart
+++ lib/layers/presentation/ui/widgets/task_card_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 import 'package:msh_checkbox/msh_checkbox.dart';
 import 'package:tasker/layers/core/injector/injector.dart';
 import 'package:tasker/layers/domain/entities/task_entity.dart';
@@ -20,9 +21,35 @@ class TaskCardWidget extends StatefulWidget {
   State<TaskCardWidget> createState() => _TaskCardWidgetState();
 }
 
-class _TaskCardWidgetState extends State<TaskCardWidget> {
+class _TaskCardWidgetState extends State<TaskCardWidget>
+    with TickerProviderStateMixin {
   final TaskController taskController = serviceLocator.get<TaskController>();
   bool isCollapsed = false;
+
+  late AnimationController controller;
+  late Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = new AnimationController(
+      duration: new Duration(milliseconds: 250),
+      vsync: this,
+    );
+
+    final CurvedAnimation curve =
+        new CurvedAnimation(parent: controller, curve: Curves.easeOut);
+
+    animation = new Tween(begin: 0.0, end: 1.0).animate(curve)
+      ..addListener(() => setState(() {}));
+    controller.forward(from: 0.0);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,6 +98,7 @@ class _TaskCardWidgetState extends State<TaskCardWidget> {
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 20),
                       child: MSHCheckbox(
+                        duration: const Duration(milliseconds: 250),
                         size: 30,
                         value: widget.task.isDone,
                         colorConfig:
@@ -79,6 +107,7 @@ class _TaskCardWidgetState extends State<TaskCardWidget> {
                         ),
                         style: MSHCheckboxStyle.fillScaleColor,
                         onChanged: (selected) {
+                          controller.forward(from: 0.0);
                           taskController
                               .changeTaskCompletionStatus(widget.task);
                         },
@@ -90,21 +119,37 @@ class _TaskCardWidgetState extends State<TaskCardWidget> {
                 backgroundColor: kCardCollapsedBackground,
                 collapsedTextColor: kSecondaryColor,
                 textColor: kSecondaryColor,
-                title: Padding(
-                  padding: const EdgeInsets.only(bottom: 4.0),
-                  child: Text(
-                    widget.task.title,
-                    style: GoogleFonts.poppins(
-                      color: kSecondaryColor,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 16,
-                      decoration: widget.task.isDone
-                          ? TextDecoration.lineThrough
-                          : TextDecoration.none,
-                      decorationColor: kSecondaryColor,
-                      decorationThickness: 2,
+                title: Stack(
+                  children: [
+                    Text(
+                      widget.task.title.trim(),
+                      style: GoogleFonts.poppins(
+                        color: kSecondaryColor,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16,
+                      ),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 4.0),
+                      child: Container(
+                        transform: Matrix4.identity()
+                          ..scale(animation.value, 1.0),
+                        child: Text(
+                          widget.task.title.trim(),
+                          style: GoogleFonts.poppins(
+                            color: Colors.transparent,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16,
+                            decoration: widget.task.isDone
+                                ? TextDecoration.lineThrough
+                                : TextDecoration.none,
+                            decorationColor: kSecondaryColor,
+                            decorationThickness: 2,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
                 childrenPadding: const EdgeInsets.symmetric(vertical: 10),
                 subtitle: Row(
@@ -117,11 +162,11 @@ class _TaskCardWidgetState extends State<TaskCardWidget> {
                         color: widget.task.isDone
                             ? kSecondaryColor.withOpacity(0.3)
                             : difference > 4
-                                ? Colors.green.withOpacity(0.3)
+                                ? kGreenCardFlag.withOpacity(0.3)
                                 : difference > 2
-                                    ? Colors.yellow.withOpacity(0.3)
+                                    ? kYellowCardFlag.withOpacity(0.3)
                                     : difference >= 0
-                                        ? Colors.red.withOpacity(0.3)
+                                        ? kRedCardFlag.withOpacity(0.3)
                                         : kSecondaryColor.withOpacity(0.3),
                         child: Center(
                           child: ClipRRect(
@@ -132,11 +177,11 @@ class _TaskCardWidgetState extends State<TaskCardWidget> {
                               color: widget.task.isDone
                                   ? kSecondaryColor
                                   : difference > 4
-                                      ? Colors.green
+                                      ? kGreenCardFlag
                                       : difference > 2
-                                          ? Colors.yellow
+                                          ? kYellowCardFlag
                                           : difference >= 0
-                                              ? Colors.red
+                                              ? kRedCardFlag
                                               : kSecondaryColor,
                             ),
                           ),
@@ -149,9 +194,15 @@ class _TaskCardWidgetState extends State<TaskCardWidget> {
                     Text(
                       widget.task.isDone
                           ? 'Done'
-                          : (!widget.task.isDone && difference >= 0)
-                              ? '${difference} days left'
-                              : 'Expired',
+                          : difference == 0
+                              ? 'Today'
+                              : (!widget.task.isDone &&
+                                      difference >= 0 &&
+                                      isCollapsed)
+                                  ? '${DateFormat("MMMM").format(widget.task.expirationDate)}, ${widget.task.expirationDate.day}'
+                                  : (!widget.task.isDone && difference >= 0)
+                                      ? '${difference} days left'
+                                      : 'Expired',
                       style: GoogleFonts.poppins(
                         color: kDarkGrey,
                         fontSize: 14,
@@ -178,7 +229,7 @@ class _TaskCardWidgetState extends State<TaskCardWidget> {
                         child: Padding(
                           padding: const EdgeInsets.fromLTRB(20, 0, 20, 12),
                           child: Text(
-                            widget.task.description,
+                            widget.task.description.trim(),
                             style: GoogleFonts.poppins(
                               color: kSecondaryColor,
                               fontSize: 14,
